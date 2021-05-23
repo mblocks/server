5f06c7e758b1f45398ccbffe988eda56b83f70b2
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-05-23 23:44:03 +0800

    Update test_crud.py

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -4,16 +4,23 @@ from app.db import crud
 from app.schemas import AppCreate, AppUpdate, Service, Environment, UserCreate, Role, RoleCreate
 
 def test_app_create(db):
-    app = crud.app.create(db=db, obj_in=AppCreate(
-        name='hello1',
-        title='hello1',
-        enabled=True,
-        endpoint='http://10.12.32.54',
-        services=[
-            Service(name='main',title='main-title',image='nginx:alpine',environment=[Environment(name='DB_HOST',value='127.0.0.1')]),
-            Service(name='other',title='other-title',image='nginx:alpine')
-        ]
-    ))
+    app = crud.app.create(db=db, obj_in=AppCreate.parse_obj({
+            'name': 'hello1',
+            'title': 'hello1',
+            'services':[
+                {   'name':'main',
+                    'title':'main-title',
+                    'image':'nginx:alpine',
+                    'environment':[
+                        {'name':'DB_HOST','value':'127.0.0.1'}
+                    ]
+                },
+                {   'name':'other',
+                    'title':'other-title',
+                    'image':'nginx:alpine'
+                }
+            ]
+        }))
     find_app = crud.app.get(db=db, id=app.id)
     first_service = find_app.services[0]
     assert find_app.name == app.name
@@ -22,35 +29,31 @@ def test_app_create(db):
     assert isinstance(first_service.environment ,list)
 
 def test_app_update(db):
-    app = crud.app.create(db=db, obj_in=AppCreate(
-        name='hello2',
-        title='hello2',
-        enabled=True,
-        endpoint='http://10.12.32.54',
-        services=[]
-    ))
+    app = crud.app.create(db=db, obj_in=AppCreate.parse_obj({
+        'name':'hello2',
+        'title':'hello2',
+        'services':[]
+    }))
     crud.app.update(db=db, db_obj=app, obj_in=AppUpdate(name='hello2-updated'))
     find_app = crud.app.get(db=db, id=app.id)
     assert app.name == find_app.name
 
 def test_app_remove(db):
-    app = crud.app.create(db=db, obj_in=AppCreate(
-        name='hello3',
-        title='hello3',
-        enabled=True,
-        endpoint='http://10.12.32.54',
-        services=[]
-    ))
+    app = crud.app.create(db=db, obj_in=AppCreate.parse_obj({
+        'name':'hello3',
+        'title':'hello3',
+        'services':[]
+    }))
     crud.app.remove(db=db, id=app.id)
     find_app = crud.app.get(db=db, id=app.id)
     assert find_app is None
 
 def test_app_role_create(db):
-    role = crud.role.create(db=db, obj_in=RoleCreate(
-        parent_id=1,
-        title='role1',
-        auth='{"query":200}'
-    ))
+    role = crud.role.create(db=db, obj_in=RoleCreate.parse_obj({
+        'parent_id':1,
+        'title':'role1',
+        'auth':'{"query":200}'
+    }))
     find_role = crud.role.get(db=db, id=role.id)
     assert role.title == find_role.title
 
