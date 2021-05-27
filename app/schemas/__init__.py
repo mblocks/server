6e2479532a99921649d35b2b9e90e6634e69afb2
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-05-27 21:42:19 +0800

    Update __init__.py

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,4 @@
-from .app import App, AppCreate, AppUpdate
+from .app import App, AppCreate, AppUpdate, AppList
 from .service import Service, ServiceCreate, ServiceUpdate, Environment
 from .user import UserCreate, UserUpdate, AppRoles
 from .role import Role, RoleCreate, RoleUpdate
