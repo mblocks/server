c47d7f3d83066b944233fa65696588dc91bf846a
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-06-04 00:34:48 +0800

    Update usercenter.py

--- app/routers/usercenter.py
+++ app/routers/usercenter.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
-from fastapi import APIRouter, Depends
-from app import config
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import config, deps, schemas
+from app.utils import verify_password
+from app.db import crud
 
 router = APIRouter()
 
-@router.get("/whoami")
-async def get_current_user(settings: config.Settings = Depends(config.get_settings)):
+
+@router.get('/whoami')
+async def whoami(settings: config.Settings = Depends(config.get_settings)):
     return {
         'logo': 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
         'title': 'Welcome',
@@ -14,3 +18,35 @@ async def get_current_user(settings: config.Settings = Depends(config.get_settin
             'user_name':'myname'
         }
     }
+
+@router.get('/settings/userinfo', response_model=schemas.Userinfo)
+async def get_userinfo(current_user = Depends(deps.get_current_user), 
+                       db: Session = Depends(deps.get_db)
+                      ):
+    find_account = crud.account.get(db, id=current_user.id)
+    return find_account
+
+@router.post('/settings/userinfo', response_model=schemas.Userinfo)
+async def update_userinfo(payload: schemas.Userinfo,
+                          current_user = Depends(deps.get_current_user), 
+                          db: Session = Depends(deps.get_db)
+                         ):
+    find_account = crud.account.get(db, id=current_user.id)
+    return crud.account.update(db, db_obj=find_account, obj_in=payload)
+
+@router.post('/settings/security/password')
+async def update_password(payload: schemas.AccountChangePassword,
+                          current_user = Depends(deps.get_current_user),
+                          db: Session = Depends(deps.get_db)
+                        ):
+    find_account = crud.account.get(db, id=current_user.id)
+    if not verify_password(payload.password, find_account.password):
+        raise HTTPException(status_code=422, detail=[
+            {
+                "loc": ["body", "password"],
+                "msg": "password is correct",
+                "type": "value_error"
+            },
+        ])
+    crud.account.update(db, db_obj=find_account, obj_in={'password':payload.new_password})
+    return { 'result': True }
