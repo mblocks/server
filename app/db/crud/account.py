34b5b6f61a9ebdfad1a55bea966e516280d520d2
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-06-03 21:48:51 +0800

    Update account.py

--- app/db/crud/account.py
+++ app/db/crud/account.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+from sqlalchemy.orm import Session
 from app.db.models import User
 from app.schemas import AccountCreate, AccountUpdate
 from .base import CRUDBase
 
 class CRUDAccount(CRUDBase[User, AccountCreate, AccountUpdate]):
-    pass
+    def before_create(self, db: Session, *, obj_in):
+        if obj_in.display_name is None:
+            obj_in.display_name = obj_in.user_name
 
 account = CRUDAccount(User)
