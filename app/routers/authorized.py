1b8fb384078e0a8094641ce39d8b5b93765cae23
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-06-06 14:53:56 +0800

    Update authorized.py

--- app/routers/authorized.py
+++ app/routers/authorized.py
@@ -1,17 +1,10 @@
 # -*- coding: utf-8 -*-
-from typing import List
-from fastapi import APIRouter, Depends, Query
-from app import deps
+from fastapi import APIRouter
 from app.db import cache
-
 
 
 router = APIRouter()
 
 @router.get("/authorized")
-async def get_authorized(user_id: int,
-                         app_id: List[int] = Query(...),
-                         redis_client = Depends(deps.get_redis)
-                        ):
+async def get_authorized(user_id: int):
     return cache.get_authorized(user_id=user_id)
-
