cbf385cf407c01fc7c46ea247dc340760d0ebf7f
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-06-10 23:56:40 +0800

    Update initial_services.py

--- scripts/initial_services.py
+++ scripts/initial_services.py
@@ -1,108 +1,133 @@
-import os
+import logging
 import time
 import docker
 import requests
+from app.db.session import SessionLocal
+from app.db import crud
 from app.config import get_settings
 
+
 settings = get_settings()
-
-#client = docker.from_env()
-client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 container_name_prefix = settings.CONTAINER_NAME_PREFIX
-mock = False
-plugins = []
-services = {
-    'gateway': {
-        'image': 'mblocks/gateway',
-        'settings': {
-            'ports': {'80': '80','8001':'8001'},
-            'environment': {
-                'KONG_DATABASE': 'off',
-                'KONG_PROXY_ACCESS_LOG': '/dev/stdout',
-                'KONG_PROXY_ERROR_LOG': '/dev/stdout',
-                'KONG_ADMIN_ACCESS_LOG': '/dev/stdout',
-                'KONG_ADMIN_ERROR_LOG': '/dev/stdout',
-                'KONG_ADMIN_LISTEN': '0.0.0.0:8001',
-                'KONG_PLUGINS': 'bundled,redis-auth',
-            }
-        },
-    },
-    'redis': {
-        'image': 'redis:alpine',
-        'settings': {}
-    }
-}
+client = docker.from_env()
+#client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+db = SessionLocal()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-
-
-# prepare images
-for item in [v.get('image') for k,v in services.items()]:
-    try:
-        client.images.get(item)
-    except docker.errors.ImageNotFound:
-        for line in client.api.pull(item, stream=True, decode=True):
-            print(line)
-
-
-# get mblocks/server network
+network = ''
+server_ip = ''
 for item in client.containers.list(filters={'ancestor': 'mblocks/server'}):
-    server_network = list(item.attrs['NetworkSettings']['Networks'].keys())[0]
-    server_ip = item.attrs['NetworkSettings']['Networks'][server_network]['IPAddress']
+    network = list(item.attrs['NetworkSettings']['Networks'].keys())[0]
+    server_ip = item.attrs['NetworkSettings']['Networks'][network]['IPAddress']
 
-
-
-# create container if not exists
-for name, item in services.items():
+if network == '':
     try:
-        client.containers.get(container_name_prefix + name)
-    except docker.errors.NotFound:
-        client.containers.run(item.get('image'),
-                              name=container_name_prefix + name,
-                              detach=True,
-                              network=server_network,
-                              **item.get('settings')
-                              )
+        raise ValueError
+    except ValueError:
+        print("Can not find docker image mblocks/server running")
 
-# get mblocks's container ip adress
-for item in client.containers.list(all=True, filters={'name': container_name_prefix}):
-    # start exited container
-    if item.status == 'exited':
-        item.start()
-    # get container's ip
-    services[item.name.replace(container_name_prefix, '')]['ip'] = item.attrs['NetworkSettings']['Networks'][server_network]['IPAddress']
+def deploy_stack(client, *, network, stack, server_ip,prefix: str):
+    stack_status = {}
+    # prepare images
+    for item in [v.image for v in stack.services]:
+        try:
+            client.images.get(item)
+        except docker.errors.ImageNotFound:
+            for line in client.api.pull(item, stream=True, decode=True):
+                print(line)
 
-
-if services.get('redis'):
-    with open('/etc/hosts', 'a') as f:
-        print('{} redis'.format(services['redis']['ip']), file=f)  # Python 3.x
-
-
-
-
-kong_config = {
-    "_format_version": "2.1",
-    "_transform": True,
-    "services":[
-        {
-            "name": "server",
-            "url": 'http://{}/'.format(server_ip),
-            "routes": [ { "paths": ["/"] } ]
+    # create container if not exists
+    for item in stack.services:
+        item_container_name = '{}-{}-{}'.format(prefix, stack.name, item.name)
+        item_settings = {
+            'ports': {'80': '80'} if stack.name =='server' and item.name == 'gateway' else {},
+            'environment':{item_env.get('name'):item_env.get('value') for item_env in item.environment } # translate list of object to dict
         }
-    ],
-    "plugins":[
-        {
-          "name":"redis-auth",
-          "config":{
-                    "hide_credentials": True,
-                    "redis_host": services['redis']['ip'],
-                    "redis_key_prefix": "redis-auth:",
-                    "consumer_keys": ["id", "third", "third_user_id", "third_user_name","is_admin"],
-                    "anonymous": True,
-                    "anonymous_paths": ["/welcome"]
-          }
-      }
-    ]
-}
-time.sleep(3)
-print(kong_config)
-resp = requests.post(('http://{}:8001/config'.format(services['gateway']['ip'])), json=kong_config)
+
+        stack_status[item.name] = {'ip':'','container_id':''}
+        if item.image == 'mblocks/server':
+            stack_status[item.name]['ip'] = server_ip
+            continue
+        
+        try:
+            client.containers.get(item_container_name)
+        except docker.errors.NotFound:
+            item_image = client.images.get(item.image)
+            if item_image.attrs['Config']['Volumes']:
+                for item_image_volume in item_image.attrs['Config']['Volumes'].keys():
+                    item_settings['volumes'] = {'/mblocks/{}/{}{}'.format(stack.name, item.name, item_image_volume): {'bind': item_image_volume, 'mode': 'rw'}}
+            client.containers.run(item.image,
+                                name=item_container_name,
+                                detach=True,
+                                network=network,
+                                hostname=item_container_name,
+                                **item_settings
+                                )
+    
+    # get stack's container ip adress
+    for item in client.containers.list(all=True, filters={'name': '{}-{}-'.format(prefix, stack.name)}):
+        # get container's ip
+        stack_status[item.name.replace('{}-{}-'.format(prefix, stack.name), '')] = {
+            'ip':item.attrs['NetworkSettings']['Networks'][network]['IPAddress'],
+            'container_id':item.id
+        }
+
+    for item in stack.services:
+        print(item)
+        item.ip = stack_status[item.name]['ip']
+        item.container_id = stack_status[item.name]['container_id']
+    
+    stack.entrypoint = 'http://{}'.format(stack_status['main']['ip'])
+    return {'name':stack.name,'url':stack.entrypoint,'routes':[{'paths':[stack.path]}]}
+
+
+def refresh_apps():
+    gateway_services = []
+    for app in crud.app.get_multi(db=db,search={}):
+        gateway_services.append(deploy_stack(client=client,network=network,stack=app,server_ip=server_ip,prefix=container_name_prefix))
+    db.commit()
+    #get app server, find redis,gateway ip address
+    server = crud.app.get(db=db,id=1)
+
+    for item in server.services:
+        if item.name == 'redis':
+            server_redis_ip = item.ip
+        if item.name == 'gateway':
+            server_gateway_ip = item.ip
+
+    # write redis ip to /etc/hosts
+    with open('/etc/hosts', 'a') as f:
+        print('{} redis'.format(server_redis_ip), file=f)  # Python 3.x
+
+    kong_config = {
+        "_format_version": "2.1",
+        "_transform": True,
+        "services":gateway_services,
+        "plugins":[
+            {
+            "name":"redis-auth",
+            "config":{
+                        "hide_credentials": True,
+                        "redis_host": server_redis_ip,
+                        "redis_key_prefix": "redis-auth:",
+                        "consumer_keys": ["id", "third", "third_user_id", "third_user_name","is_admin"],
+                        "anonymous": True,
+                        "anonymous_paths": ["/welcome"]
+            }
+        }
+        ]
+    }
+
+    time.sleep(3)
+    requests.post(('http://{}:8001/config'.format(server_gateway_ip)), json=kong_config)
+
+
+def main() -> None:
+    logger.info("Creating initial services")
+    refresh_apps()
+    logger.info("Initial services created")
+
+
+if __name__ == "__main__":
+    main()
