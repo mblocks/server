1a2390b74fe31cf510cf723de31d327e493bb7fc
jooonwood <jooonwood@gmail.com>
2021-06-18 23:19:48 +0800

    !

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi:python3.8-slim
 COPY . /app
-VOLUME ["/mblocks/server"]
+VOLUME ["/mblocks"]
 RUN pip install -r requirements.txt --no-cache-dir
--- prestart.sh
+++ prestart.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env sh
 
+# create the path for sqlite data
+mkdir -p /mblocks/server/main
 python scripts/initial_database.py
 python scripts/initial_data.py
 python scripts/initial_services.py
