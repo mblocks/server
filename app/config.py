6b8e8d0a89ff3a96ffe7d47dfdd264c357ff5a97
jooonwood <45617392+jooonwood@users.noreply.github.com>
2021-10-10 18:12:45 +0800

    Update config.py

--- app/config.py
+++ app/config.py
@@ -16,16 +16,13 @@ else:
 
 
 class Settings(BaseSettings):
-    FASTAPI_CONFIG: str = os.getenv("FASTAPI_CONFIG", "development")
+    FASTAPI_CONFIG: str = 'development'
     APP_NAME: str = "Mblocks Server"
     OPENAPI_PREFIX: str = ""
-    SQLALCHEMY_DATABASE_URI: str = os.getenv(
-                                            "SQLALCHEMY_DATABASE_URI",
-                                            prefix + os.path.join(basedir, 'data.db')
-                                            )
-    SQLALCHEMY_ECHO: bool = os.getenv('SQLALCHEMY_ECHO',FASTAPI_CONFIG!='production')
-    REDIS_HOST: str = os.getenv('REDIS_HOST','admin-redis')
-    REDIS_PORT: int = os.getenv('REDIS_PORT',6379)
+    SQLALCHEMY_DATABASE_URI: str = prefix + os.path.join(basedir, 'data.db')
+    SQLALCHEMY_ECHO: bool = FASTAPI_CONFIG!='production'
+    REDIS_HOST: str = 'admin-redis'
+    REDIS_PORT: int = 6379
     REDIS_DB: int = os.getenv('REDIS_DB',0)
     REDIS_PASSWORD: str = os.getenv('REDIS_PASSWORD')
 
@@ -38,12 +35,12 @@ class Settings(BaseSettings):
 
 
 class Production(Settings):
-    SQLALCHEMY_DATABASE_URI: str = os.getenv("SQLALCHEMY_DATABASE_URI","sqlite://///mblocks/server/main/data.db")
+    SQLALCHEMY_DATABASE_URI: str = 'sqlite://///mblocks/server/main/data.db'
 
 
 class Test(Settings):
-    REDIS_HOST: str = os.getenv('REDIS_HOST','127.0.0.1')
-    SQLALCHEMY_DATABASE_URI: str = os.getenv("SQLALCHEMY_DATABASE_URI","sqlite://")
+    REDIS_HOST: str = '127.0.0.1'
+    SQLALCHEMY_DATABASE_URI: str = 'sqlite:///test.db'
 
 @lru_cache()
 def get_settings():
